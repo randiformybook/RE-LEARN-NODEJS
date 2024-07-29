b89fda8b743d089bdfba4328562dc7ed9d631d43
randiformybook <randi.mybook@gmail.com>
2024-07-29 17:15:33 +0700

    asdasd

--- 3-NPM-BASIC/app.js
+++ 3-NPM-BASIC/app.js
@@ -2,5 +2,5 @@ const validator = require("validator");
 const chalk = require("chalk");
 const { default: isEmail } = require("validator/lib/isEmail");
 
-console.log(validator, isEmail(""));
-console.log(chalk.blue("Hello world!"));
+console.log(validator, isEmail("randi@email.com"));
+console.log(chalk.bgRed.white("Hello world!"));
