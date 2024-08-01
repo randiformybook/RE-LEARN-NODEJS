eb0655f46106c21eea6ba001ca6a0be333ddab97
randiformybook <randi.mybook@gmail.com>
2024-08-01 13:13:26 +0700

    "Added validator and chalk dependencies, updated contact.json file, and modified system.js to include input validation using validator and chalk for colored console output."

--- 5-CREATE-CONTACT-APP (2)/APP/system.js
+++ 5-CREATE-CONTACT-APP (2)/APP/system.js
@@ -2,6 +2,8 @@ const fs = require("node:fs/promises");
 // Contact Folder & File
 const dirPath = "../DATA";
 const dirFile = `${dirPath}/contact.json`;
+const validator = require("validator");
+const chalk = require("chalk");
 // ----------------------
 
 // Check & Create Directory Folder
@@ -26,19 +28,43 @@ async function checkDir() {
 // Fungsi ketika pertanyaan sudah di Jawab dan Lekas di Simpan ke File Contact.Json
 async function saveFile(answer) {
   const dataJSON = await loadFile();
+
+  // validasi id
+  const id = answer.id;
+  if (validator.isNumeric(id) == false) {
+    console.log("ID harus berupa Angka");
+    return false;
+  }
+
+  // validasi No Handphone
+  const noHP = answer.noHp;
+
+  if (validator.isMobilePhone(noHP, ["id-ID"]) == false) {
+    console.log(
+      chalk.red.bold(
+        "No Handphone yang anda masukan tidak sesuai dengan Format No telepon Indonesia, Silahkan untuk check kembali.Terimah kasih !"
+      )
+    );
+    return false;
+  }
+
   // check duplicate contact
   const duplicate = dataJSON.find(
     (data) => data.nama === answer.nama || data.id === answer.id
   );
   if (duplicate) {
-    console.log("Contact Sudah Ada");
+    console.log(
+      chalk.red.bold(
+        "Contact yang anda masukin sudah terdaftar, Silahkan melakukan pengecekan ulang"
+      )
+    );
     return false;
   }
 
   dataJSON.push(answer);
   const updateBuffer = await Buffer.from(JSON.stringify(dataJSON));
   await fs.writeFile(dirFile, updateBuffer);
-  console.log("Data Anda sudah di Simpan");
+  console.log(chalk.green.bold("Data Anda sudah berhasil di Simpan"));
 }
 
 // Fungsi ketika ingin memanggil/load dari isi File contact
