d1dcdaaa0f7e0e6fdb19aa883a7ab29abaa03c09
randiformybook <randi.mybook@gmail.com>
2024-08-19 10:52:28 +0700

    pembuatan folder 11-EXPRESS-CONTACT-APP (2)

---
+++ 11-EXPRESS-CONTACT-APP (2)/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const {
+  checkDir,
+  loadFile,
+  findContact,
+} = require("./utilities/contact-system");
+
+// Menyajikan file statis dari folder 'public'
+app.use(express.static("public"));
+
+// gunakan ejs
+app.set("view engine", "ejs");
+
+// Third-party Middleware
+app.use(expressLayouts);
+
+// -------Home Page Route---------
+app.get("/", (req, res) => {
+  const karyawan = [
+    { id: 127, nama: "Randi", jabatan: "Junior Programmer" },
+    { id: 257, nama: "Putri", jabatan: "Dokter" },
+    { id: 337, nama: "Ilyas", jabatan: "Captain-Pilot" },
+  ];
+  res.render("index", {
+    layout: "layouts/main-layout.ejs",
+    title: "Halaman Home",
+    currentPath: req.path,
+    cssLink: "/",
+    karyawan,
+  });
+});
+// -------About Page Route---------
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout.ejs",
+    title: "Halaman About",
+    currentPath: req.path,
+    cssLink: "/",
+  });
+});
+// -------Contact Page Route---------
+app.get("/contact", async (req, res) => {
+  await checkDir();
+  const contacts = await loadFile();
+  res.render("contact", {
+    layout: "layouts/main-layout.ejs",
+    title: "Halaman Contact",
+    currentPath: req.path,
+    cssLink: "/",
+    contacts,
+  });
+});
+// Contact Detail by ID
+app.get("/contact/:id", async (req, res) => {
+  const contact = await findContact(req.params.id);
+  res.render("contact-detail", {
+    layout: "layouts/main-layout.ejs",
+    title: "Halaman Detail Contact",
+    cssLink: "/css/contact-detail.css",
+    contact,
+  });
+});
+// Apabila halaman tidak di temukan
+app.use("/", (req, res) => {
+  res.sendStatus(404);
+});
+
+// Hotsname & Port
+const port = "3000";
+const host = "127.0.0.1";
+
+app.listen(port, host, () => {
+  console.log(`Server running on ${host}:${port}`);
+});
---
+++ 11-EXPRESS-CONTACT-APP (2)/utilities/contact-system.js
@@ -0,0 +1,50 @@
+const fs = require("fs/promises");
+const path = require("path");
+const validator = require("validator");
+
+// location Directory File
+const dirPath = path.join("./data");
+const filePath = path.join(dirPath, "contact.json");
+
+async function checkDir() {
+  try {
+    // check apakah Folder bernama DATA
+    await fs.access(dirPath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.mkdir(dirPath, { recursive: true });
+    } else {
+      throw new Error("Gagal Pembuatan Folder : " + err);
+    }
+  }
+  try {
+    // check apakah ada terdapat file contacts.json atau tidak
+    await fs.access(filePath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, "[]", "utf-8");
+    } else {
+      throw new Error("GAGAL MEMBUAT FILE : " + err);
+    }
+  }
+}
+
+async function loadFile() {
+  const dataJson = await fs.readFile(filePath, "utf-8");
+  if (!dataJson) return false;
+  const dataBuffer = await JSON.parse(dataJson);
+  return dataBuffer;
+}
+
+async function findContact(id) {
+  const contacts = await loadFile();
+  const contactFind = await contacts.filter((contact) => contact.id == id);
+  const contact = contactFind[0];
+  return contact;
+}
+
+module.exports = {
+  checkDir,
+  loadFile,
+  findContact,
+};
